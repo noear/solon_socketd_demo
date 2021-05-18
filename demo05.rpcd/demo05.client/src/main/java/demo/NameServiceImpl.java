46f2d5daea876b19977590adb6a6e1d0b9f15c9a
noear <noear@live.cn>
2021-05-18 17:42:34 +0800

    1.4.0

--- demo05.rpcd/demo05.client/src/main/java/demo/NameServiceImpl.java
+++ demo05.rpcd/demo05.client/src/main/java/demo/NameServiceImpl.java
@@ -2,11 +2,12 @@ package demo;
 
 import org.noear.solon.annotation.Component;
 import org.noear.solon.annotation.Mapping;
+import org.noear.solon.annotation.Remoting;
 import org.noear.solon.core.handle.MethodType;
 
 //定义远程服务组件（供服务端调用）
 @Mapping(value = "/demoe/rpc/name", method = MethodType.SOCKET)
-@Component(remoting = true)
+@Remoting
 public class NameServiceImpl implements NameService {
     @Override
     public String name(String name) {
